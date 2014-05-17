252046c0769f590d1038834a6b4fa19e7f67cfbf
mohitgurnani1 <mohitgurnani1@gmail.com>
2014-05-18 00:37:48 +0530

    ServerSide of WeChat

---
+++ src/ServerSide.java
@@ -0,0 +1,29 @@
+import java.net.*;
+	import java.io.*;
+
+	class ServerSide
+	{
+private static InputStreamReader inputStreamReader;
+	private static BufferedReader bufferedReader;
+	private static String message;
+
+			
+public static void main(String args[])throws IOException
+		{
+			ServerSocket ss=new ServerSocket(4456);
+			
+				Socket client=ss.accept();
+		
+				inputStreamReader = new InputStreamReader(client.getInputStream());
+				bufferedReader = new BufferedReader(inputStreamReader); // get the client message
+				message = bufferedReader.readLine();
+
+				System.out.println(message);
+				inputStreamReader.close();
+				client.close();
+
+
+				
+		}
+	}
+
